da4118ca348163eae0bb0f94a511e9d52ff7e091
Valeria Gonzalez <valeria._mg@hotmail.com>
2024-09-16 15:07:10 -0600

    Se agrego la biblioteca de clases Manejador, en el cree la clase ManejadorAltas el cual ya contiene programado la accion del procedure insertar, en este punto ya esta referenciado y ya funciona dicha funcion

---
+++ PresentacionTienda__/Manejador/ManejadorAltas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AccesoDatos;
+using Entidades;
+
+namespace Manejador
+{
+    public class ManejadorAltas
+    {
+        Base b = new Base("localhost", "root", "", "Tienda");
+        List<Datos> dato = new List<Datos>();
+
+        public string Guardar(TextBox Nombre, TextBox Descripcion, TextBox Precio)
+        {
+            try
+            {
+                return b.Comando($"CALL p_Agregar(null, '{Nombre.Text}','{Descripcion.Text}', {double.Parse(Precio.Text)})");
+
+            }
+            catch (Exception)
+            {
+                return "Error de valor";
+            }
+        }
+
+
+    }
+}
