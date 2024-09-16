2482383a1d002831ab6b750f964d9aa68998a82a
Valeria Gonzalez <valeria._mg@hotmail.com>
2024-09-16 17:52:30 -0600

    Se modifico el PresentacionTienda__ en el cual se agrego el (FrmModificaciones), ya esta programado respecto al Manejador al igual que hace una referencia a (FrmAltas)

---
+++ PresentacionTienda__/PresentacionTienda__/FrmModificaciones.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace PresentacionTienda_
+{
+    partial class FrmModificaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.dtgvDatos = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvDatos)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(156)))), ((int)(((byte)(181)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(319, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 41);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "PRODUCTOS:";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI Semilight", 7.875F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(74)))), ((int)(((byte)(107)))));
+            this.label3.Location = new System.Drawing.Point(1338, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 25);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "MODIFICAR";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.Transparent;
+            this.btnModificar.BackgroundImage = global::PresentacionTienda__.Properties.Resources.Copia_de_ICONOS_VMG_2;
+            this.btnModificar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnModificar.Location = new System.Drawing.Point(1328, 107);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(6);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(150, 150);
+            this.btnModificar.TabIndex = 30;
+            this.btnModificar.UseVisualStyleBackColor = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI Semilight", 7.875F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(74)))), ((int)(((byte)(107)))));
+            this.label2.Location = new System.Drawing.Point(522, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(812, 25);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "BUSCA POR EL NOMBRE DE PRODUCTO Y SELECCIONA, PARA PODER MODIFICAR:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Font = new System.Drawing.Font("Microsoft JhengHei UI", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscar.Location = new System.Drawing.Point(574, 208);
+            this.txtBuscar.Margin = new System.Windows.Forms.Padding(6);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(700, 42);
+            this.txtBuscar.TabIndex = 27;
+            this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
+            // 
+            // dtgvDatos
+            // 
+            this.dtgvDatos.AllowUserToAddRows = false;
+            this.dtgvDatos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(247)))), ((int)(((byte)(218)))));
+            this.dtgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvDatos.Location = new System.Drawing.Point(277, 332);
+            this.dtgvDatos.Margin = new System.Windows.Forms.Padding(6);
+            this.dtgvDatos.Name = "dtgvDatos";
+            this.dtgvDatos.ReadOnly = true;
+            this.dtgvDatos.RowHeadersWidth = 82;
+            this.dtgvDatos.RowTemplate.Height = 33;
+            this.dtgvDatos.Size = new System.Drawing.Size(1442, 356);
+            this.dtgvDatos.TabIndex = 28;
+            this.dtgvDatos.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgvDatos_CellEnter);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI Semilight", 7.875F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(74)))), ((int)(((byte)(107)))));
+            this.label4.Location = new System.Drawing.Point(1499, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 25);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "REGRESAR";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.Transparent;
+            this.btnRegresar.BackgroundImage = global::PresentacionTienda__.Properties.Resources.ICONO_REGRESO;
+            this.btnRegresar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRegresar.Location = new System.Drawing.Point(1490, 107);
+            this.btnRegresar.Margin = new System.Windows.Forms.Padding(6);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(150, 150);
+            this.btnRegresar.TabIndex = 33;
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.dtgvDatos);
+            this.groupBox1.Controls.Add(this.btnRegresar);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnModificar);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtBuscar);
+            this.groupBox1.Location = new System.Drawing.Point(336, 95);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1899, 746);
+            this.groupBox1.TabIndex = 35;
+            this.groupBox1.TabStop = false;
+            // 
+            // FrmModificaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::PresentacionTienda__.Properties.Resources.Copia_de_TBD_PROYECTO_FINAL_YJGSVMG;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1920, 1234);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft JhengHei UI", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FrmModificaciones";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmModificaciones";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvDatos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.DataGridView dtgvDatos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ PresentacionTienda__/PresentacionTienda__/FrmModificaciones.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Manejador;
+
+namespace PresentacionTienda_
+{
+    public partial class FrmModificaciones : Form
+    {
+        ManejadorModificaciones MM;
+        int fila = 0, columna = 0;
+        public static int Id = 0;
+        public static string Nombre = "", Descripcion = "";
+        public static double Precio = 0.0;
+
+
+        public FrmModificaciones()
+        {
+            InitializeComponent();
+            MM = new ManejadorModificaciones();
+        }
+
+
+        private void dtgvDatos_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            fila = e.RowIndex; columna = e.ColumnIndex;
+        }
+
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (fila >= 0)
+            {
+                Id = int.Parse(dtgvDatos.Rows[fila].Cells[0].Value.ToString());
+                Nombre = dtgvDatos.Rows[fila].Cells[1].Value.ToString();
+                Descripcion = dtgvDatos.Rows[fila].Cells[2].Value.ToString();
+                Precio = double.Parse(dtgvDatos.Rows[fila].Cells[3].Value.ToString());
+                FrmAltas dm = new FrmAltas();
+                dm.ShowDialog();
+                Limpiar();
+                txtBuscar.Focus();
+            }
+            else
+                MessageBox.Show("Por favor, seleccione una fila válida para modificar.");
+
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            dtgvDatos.Visible = true;
+            MM.Mostrar(dtgvDatos, txtBuscar.Text);
+        }
+
+       
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        void Limpiar()
+        {
+            dtgvDatos.Visible = false;
+        }
+
+
+
+    }
+}
